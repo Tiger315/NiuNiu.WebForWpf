34fc22c1640190673affe5bbe09c1d2bfcc8548b
fan.ch <530603970@qq.com>
2018-08-24 14:07:47 +0800

    '舆情页面提交'

--- src/App.vue
+++ src/App.vue
@@ -20,4 +20,15 @@ export default {
   margin:0px;
   padding:0px;
 }
+.el-button--primary{
+  background-color: #0d308c;
+  border-color: #0d308c;
+}
+.el-pager li.active {
+    color: #0d308c;
+    cursor: default;
+}
+.el-pager li:hover {
+    color: #0d308c;
+}
 </style>
