2f7cee375be1ebede8e959fd9509f157146cfa4c
fan.ch <530603970@qq.com>
2018-08-24 14:18:33 +0800

    '舆情页面提交'

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import LawList from '@/components/law/LawList'
-import LawListForWpf from '@/components/law/LawListForWpf'
 import SupervisionType from '@/components/information/yuqing/SupervisionType' // 舆情监管动态
 import IrregularitiesType from '@/components/information/yuqing/IrregularitiesType' // 舆情违规案例
 import Feedback from '@/components/information/yuqing/Feedback' // 舆情监管函件
@@ -11,15 +9,6 @@ Vue.use(Router)
 export default new Router({
   // mode: 'history',
   routes: [
-    {
-      path: '/Law/LawList',
-      name: 'LawList',
-      component: LawList
-    }, {
-      path: '/Law/LawListForWpf',
-      name: 'LawListForWpf',
-      component: LawListForWpf
-    },
     {
       path: '/yuqing/supervisionType',
       name: ' SupervisionType',
