c3b40ef0653a697b25e7d8f7f91223e56e16834b
Zhouhu <huhu_315boy@163.com>
2018-09-13 14:07:20 +0800

    研究报告功能更新

--- src/components/info/yjbg/presentation.vue
+++ src/components/info/yjbg/presentation.vue
@@ -810,19 +810,21 @@ export default {
       let dates = date.split(' ')[0]
       return dates
     },
-    showPDF (urls) { // 展示pdf
+    showPDF (urls) {
       this.urlData.pdfUrl = urls
       var Idx = urls.indexOf('pdf')
-      if (Idx === -1) { // 判断如果不是pdf文件，调用word
+
+      // 判断如果不是pdf文件，调用word
+      if (Idx === -1) {
         this.showWord(urls)
       }
       this.dialog = true
     },
-    showWord (urls) { // 展示word
+    showWord (urls) {
       this.urlData.showWordUrl = 'https://view.officeapps.live.com/op/view.aspx?src=' + urls
       this.dialog = true
     },
-    beforeClose () { // 弹窗关闭之前清除数据
+    beforeClose () {
       this.urlData.pdfUrl = ''
       this.urlData.showWordUrl = ''
       this.dialog = false
